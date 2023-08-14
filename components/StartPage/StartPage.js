2b1661d669602dbbf2d94fd864dd2b1ee60a7727
Yon Valencion <herokiller32e@gmail.com>
2023-08-13 22:30:45 -0700

    Start menu style updates

--- components/StartPage/StartPage.js
+++ components/StartPage/StartPage.js
@@ -2,12 +2,26 @@ import styles from './StartPage.module.css';
 
 const StartPage = ({ setGameStarted }) => {
 
+    const MenuButton = ({ buttonAction, title, style }) => {
+        return (
+            <div onClick={buttonAction} className={`${styles.pushable} ${styles[style]}`}>
+                <span className={`${styles.front} ${styles[style]}`}>{title}</span>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.page}>
             <div className={styles.holder}>
-                <h1 style={{margin: "0px"}}>Welcome</h1>
-                <button onClick={() => setGameStarted(true)} className={styles["start-button"]}>Start</button>
+                <h1 className={styles.title}>Applicant.io</h1>
+                <p className={styles.info}>(Playable Alpha Verison)</p>
+                <div className={styles["button-holder"]}>
+                    <MenuButton title="Start" buttonAction={() => setGameStarted(true)} />
+                    <MenuButton style={"yellow"} title="Settings" buttonAction={() => setGameStarted(true)} />
+                </div>
+
             </div>
+            <p className={styles.verison}>Verison: Alpha 0.01</p>
         </div>
 
     );
